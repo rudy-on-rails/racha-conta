ab6dab4f5a935bce7bb7f60b8eaecb919943ca63
Rudy Seidinger <rudyseidinger@gmail.com>
2013-10-09 19:54:08 -0300

    Ignore app settings file load on envs other than dev

--- config/initializers/app_settings_loader.rb
+++ config/initializers/app_settings_loader.rb
@@ -1,1 +1,3 @@
-APPSETTINGS = YAML.load(File.read("#{Rails.root.join("config/application.yml")}"))
+if Rails.env.development?
+	APPSETTINGS = YAML.load(File.read("#{Rails.root.join("config/application.yml")}"))
+end
